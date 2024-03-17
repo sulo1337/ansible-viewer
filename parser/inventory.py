81d5624d9a789bd7e6f3d357b7c7a9a53900bdba
sulo1337 <sulochan.acharya2@gmail.com>
2024-03-17 12:29:34 -0500

    feat: parse other groups as well as all group

--- parser/inventory.py
+++ parser/inventory.py
@@ -14,49 +14,59 @@ class InventoryParser:
             with open(filepath, 'r') as f:
                 inventory = yaml.safe_load(f)
                 
-            if not inventory or 'all' not in inventory:
+            if not inventory:
                 return False
+
+            # Process all top-level groups including 'all'
+            for group_name, group_data in inventory.items():
+                self._process_group(group_name, group_data)
                 
-            # Process hosts in 'all' group
-            if 'hosts' in inventory['all']:
-                for hostname, host_vars in inventory['all']['hosts'].items():
-                    if hostname not in self.hosts:
-                        self.hosts[hostname] = {
-                            'groups': ['all'],
-                            'vars': {}
-                        }
-                    if host_vars:
-                        self.hosts[hostname]['vars'].update(host_vars)
-            
-            # Process children groups
-            if 'children' in inventory['all']:
-                for group_name, group_data in inventory['all']['children'].items():
-                    if group_name not in self.groups:
-                        self.groups[group_name] = {
-                            'hosts': [],
-                            'vars': {},
-                            'children': []
-                        }
-                    
-                    if 'hosts' in group_data:
-                        for hostname in group_data['hosts'].keys():
-                            self.groups[group_name]['hosts'].append(hostname)
-                            if hostname not in self.hosts:
-                                self.hosts[hostname] = {
-                                    'groups': ['all', group_name],
-                                    'vars': {}
-                                }
-                            else:
-                                self.hosts[hostname]['groups'].append(group_name)
-                    
-                    if 'vars' in group_data:
-                        self.groups[group_name]['vars'].update(group_data['vars'])
-            
             return True
             
         except Exception as e:
             print(f"Error parsing inventory file {filepath}: {str(e)}")
             return False
+
+    def _process_group(self, group_name, group_data):
+        # Initialize group if not exists
+        if group_name not in self.groups:
+            self.groups[group_name] = {
+                'hosts': [],
+                'vars': {},
+                'children': []
+            }
+
+        # Process group vars
+        if 'vars' in group_data:
+            self.groups[group_name]['vars'].update(group_data['vars'])
+
+        # Process direct hosts in group
+        if 'hosts' in group_data:
+            for hostname, host_vars in group_data['hosts'].items():
+                self.groups[group_name]['hosts'].append(hostname)
+                if hostname not in self.hosts:
+                    self.hosts[hostname] = {
+                        'groups': [group_name],
+                        'vars': {}
+                    }
+                else:
+                    if group_name not in self.hosts[hostname]['groups']:
+                        self.hosts[hostname]['groups'].append(group_name)
+                if host_vars:
+                    self.hosts[hostname]['vars'].update(host_vars)
+
+        # Process children groups recursively
+        if 'children' in group_data:
+            for child_name, child_data in group_data['children'].items():
+                self.groups[group_name]['children'].append(child_name)
+                self._process_group(child_name, child_data)
+                # Add hosts from child groups to parent group's host list
+                if child_name in self.groups:
+                    for hostname in self.groups[child_name]['hosts']:
+                        if hostname not in self.groups[group_name]['hosts']:
+                            self.groups[group_name]['hosts'].append(hostname)
+                        if hostname in self.hosts and group_name not in self.hosts[hostname]['groups']:
+                            self.hosts[hostname]['groups'].append(group_name)
             
     def get_host_groups(self, hostname):
         if hostname in self.hosts:
